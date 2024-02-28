4f5c0007e251f5343f82cbcb3ae9a9bcccc132c8
habiba333 <habiba.mo.ali3@gmail.com>
2024-02-28 13:18:24 +0200

    finished project

---
+++ lib/ques_ans_file.dart
@@ -0,0 +1,42 @@
+class Flashcard {
+  final String question;
+  final String answer;
+
+  Flashcard({required this.question, required this.answer});
+}
+
+List<Flashcard> quesAnsList = [
+  Flashcard(
+      question: "Q.What is the currency of Japan?",
+      answer: "Answer: Japanese Yen."),
+  Flashcard(
+      question: "Q. What is the largest mammal in the world?",
+      answer: "Answer: Blue Whale."),
+  Flashcard(
+      question: "Q.Who discovered penicillin?",
+      answer: "Answer: Alexander Fleming."),
+  Flashcard(
+      question:
+          "Q.What is the capital city of Australia?",
+      answer:
+          "Answer: Canberra."),
+  Flashcard(
+      question:
+          "Q.Who painted the Mona Lisa?",
+      answer: "Answer: Leonardo da Vinci."),
+  Flashcard(
+      question: "Q. What is the tallest mountain in the world?",
+      answer: "Answer: Mount Everest."),
+  Flashcard(
+      question: "Q. Who wrote the play (Romeo and Juliet)?",
+      answer: "Answer: William Shakespeare."),
+  Flashcard(
+      question: "Q.What is the chemical symbol for water?",
+      answer: "Answer: H2O."),
+  Flashcard(
+      question: "Q.What is the boiling point of water in Celsius?",
+      answer: "Answer: 100 degrees Celsius."),
+  Flashcard(
+      question: "Q.Who was the first person to step on the moon?",
+      answer: "Answer: Neil Armstrong"),
+];
